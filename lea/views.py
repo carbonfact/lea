11faa06e86f8a010d3c1f5af01e3bec035428f82
David Ruyer <david.ruyer@gmail.com>
2023-07-05 16:50:35 +0200

    Rename flag to FREEZE_RELEASES

--- lea/views.py
+++ lea/views.py
@@ -82,7 +82,7 @@ class SQLView(View):
             environment = jinja2.Environment(loader=loader)
             template = environment.get_template(str(self.relative_path))
             return template.render(
-                live_carbonverses=os.environ.get("STABLE_CARBONVERSES", "false").lower()
+                frozen_data=os.environ.get("FREEZE_RELEASES", "false").lower()
                 == "true"
             )
         return text
