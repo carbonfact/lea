aa8adefa3f0a24d09cd9b5531b35cd41ab1617a3
Max Halford <maxhalford25@gmail.com>
2023-11-17 12:41:23 +0100

    fix docs generation

--- lea/app/docs.py
+++ lea/app/docs.py
@@ -26,6 +26,8 @@ def docs(
 
     # List all the columns
     columns = client.get_columns()
+    # HACK
+    columns["view_name"] = f'{client.dataset_name}.' + columns["view_name"]
 
     # Now we can generate the docs for each schema and view therein
     readme_content = io.StringIO()
