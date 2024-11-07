f07cc2c68df8faa323e5c12bb657c8c83b318aa9
Max Halford <maxhalford25@gmail.com>
2024-11-07 07:41:55 +0100

    Create test_big_query.py

---
+++ lea/test_big_query.py
@@ -0,0 +1,170 @@
+import re
+import pytest
+
+from lea.conductor import Session
+from lea.scripts import TableRef, Script
+from lea.dialects import BigQueryDialect
+
+
+@pytest.fixture
+def scripts() -> dict[TableRef, Script]:
+    return {
+        script.table_ref: script
+        for script in [
+            Script(
+                table_ref=TableRef("read", ("raw",), "users"),
+                code="""
+                SELECT * FROM UNNEST([
+                    STRUCT(1 AS id, 'Alice' AS name, 30 AS age),
+                    STRUCT(2 AS id, 'Bob' AS name, 25 AS age),
+                    STRUCT(3 AS id, 'Charlie' AS name, 35 AS age)
+                ])
+                """,
+                sql_dialect=BigQueryDialect
+            ),
+            Script(
+                table_ref=TableRef("read", ("core",), "users"),
+                code="""
+                SELECT
+                    id,
+                    -- #INCREMENTAL
+                    name,
+                    age
+                FROM read.raw__users
+                """,
+                sql_dialect=BigQueryDialect
+            ),
+            Script(
+                table_ref=TableRef("read", ("analytics",), "n_users"),
+                code="""
+                SELECT COUNT(*)
+                FROM read.core__users
+                """,
+                sql_dialect=BigQueryDialect
+            )
+        ]
+    }
+
+
+def assert_queries_are_equal(query1: str, query2: str):
+    normalized_query1 = re.sub(r'\s+', ' ', query1).strip()
+    normalized_query2 = re.sub(r'\s+', ' ', query2).strip()
+    assert normalized_query1 == normalized_query2
+
+
+def test_simple_run(scripts):
+
+    session = Session(
+        database_client=None,
+        base_dataset="read",
+        write_dataset="write",
+        scripts=scripts,
+        selected_table_refs=scripts.keys(),
+        defer_mode=False
+    )
+
+    assert_queries_are_equal(
+        session.add_context(scripts[TableRef("read", ("raw",), "users")]).code,
+        """
+        SELECT * FROM UNNEST([
+            STRUCT(1 AS id, 'Alice' AS name, 30 AS age),
+            STRUCT(2 AS id, 'Bob' AS name, 25 AS age),
+            STRUCT(3 AS id, 'Charlie' AS name, 35 AS age)
+        ])
+        """
+    )
+    assert_queries_are_equal(
+        session.add_context(scripts[TableRef("read", ("analytics",), "n_users")]).code,
+        """
+        SELECT COUNT(*)
+        FROM write.core__users___audit
+        """
+    )
+
+
+def test_defer_mode(scripts):
+    """
+
+    In defer mode, unselected table references are replaced with production data references.
+
+    """
+
+    session = Session(
+        database_client=None,
+        base_dataset="read",
+        write_dataset="write",
+        scripts=scripts,
+        selected_table_refs={TableRef("read", ("analytics",), "n_users")},
+        defer_mode=True
+    )
+
+    assert_queries_are_equal(
+        session.add_context(scripts[TableRef("read", ("raw",), "users")]).code,
+        """
+        SELECT * FROM UNNEST([
+            STRUCT(1 AS id, 'Alice' AS name, 30 AS age),
+            STRUCT(2 AS id, 'Bob' AS name, 25 AS age),
+            STRUCT(3 AS id, 'Charlie' AS name, 35 AS age)
+        ])
+        """
+    )
+    assert_queries_are_equal(
+        session.add_context(scripts[TableRef("read", ("analytics",), "n_users")]).code,
+        """
+        SELECT COUNT(*)
+        FROM read.core__users
+        """
+    )
+
+
+
+
+def test_defer_mode_select_all(scripts):
+
+    session = Session(
+        database_client=None,
+        base_dataset="read",
+        write_dataset="write",
+        scripts=scripts,
+        selected_table_refs=scripts.keys(),
+        defer_mode=True
+    )
+
+    assert_queries_are_equal(
+        session.add_context(scripts[TableRef("read", ("raw",), "users")]).code,
+        """
+        SELECT * FROM UNNEST([
+            STRUCT(1 AS id, 'Alice' AS name, 30 AS age),
+            STRUCT(2 AS id, 'Bob' AS name, 25 AS age),
+            STRUCT(3 AS id, 'Charlie' AS name, 35 AS age)
+        ])
+        """
+    )
+    assert_queries_are_equal(
+        session.add_context(scripts[TableRef("read", ("analytics",), "n_users")]).code,
+        """
+        SELECT COUNT(*)
+        FROM write.core__users___audit
+        """
+    )
+
+
+def test_incremental_field(scripts):
+
+    session = Session(
+        database_client=None,
+        base_dataset="read",
+        write_dataset="write",
+        scripts=scripts,
+        selected_table_refs=scripts.keys(),
+        defer_mode=False,
+        incremental_field_name="name",
+        incremental_field_values={"Alice"}
+    )
+
+    assert_queries_are_equal(
+        session.add_context(scripts[TableRef("read", ("core",), "users")]).code,
+        """
+
+        """
+    )
