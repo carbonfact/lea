350ba9ac3270a9e44c0567da71411ac6f170d9f7
Max Halford <maxhalford25@gmail.com>
2024-11-19 12:26:52 +0100

    Update __init__.py

--- lea/__init__.py
+++ lea/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
-from lea import databases
+from lea import cli, databases
 from lea.conductor import Conductor
-from lea import cli
 
 __all__ = ["cli", "Conductor", "databases"]
