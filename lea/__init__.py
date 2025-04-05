6cbfc39bc64e35bca4d22c7d8c8971d0b6d64ffb
Max Halford <maxhalford25@gmail.com>
2025-04-05 15:30:43 +0200

    Update __init__.py

--- lea/__init__.py
+++ lea/__init__.py
@@ -5,7 +5,7 @@ import logging
 import click
 from rich.logging import RichHandler
 
-from lea import cli, databases, scripts
+from lea import cli, databases
 from lea.conductor import Conductor
 
 logging.basicConfig(
